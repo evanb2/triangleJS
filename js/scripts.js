7354b33613a905a8157c6c50339593e958fc3f0f
Patrick Fiorentino & Evan Butler <student@epicodus.com>
2015-04-14 15:02:59 -0700

    move append out of method, create isError property, clean code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-
-
-
 $(document).ready(function() {
     $("#sideA").focus();
     $("form#trianglecheck").submit(function(event) {
@@ -12,48 +9,53 @@ $(document).ready(function() {
             A: sideA,
             B: sideB,
             C: sideC,
+            isError: false,
             triangleCheck: function() {
                 var sides = [this.A, this.B, this.C];
                 sides.sort(function(a, b){return a-b});
                 if (this.A === 0 && this.B === 0 && this.C === 0) {
+                    this.isError = true;
                     return "error, must enter a number greater then 0 for all inputs";
                 } else if (this.A === 0 || this.B === 0 || this.C === 0) {
+                    this.isError = true;
                     return "error, all inputs must be greater than 0";
                 } else if (sides[0] + sides[1] < sides[2]) {
+                    this.isError = true;
                     return "error, one input cannot equal more than the other two combined";
 
                 } else if (this.A === this.B && this.B === this.C) {
-                    $('#equilateral').append("<li>" + this.A + ", " + this.B + ", " + this.C + "</li>");
-                    return "equilateral triangle";
+                    return "equilateral";
 
                 } else if ((this.A === this.B) || (this.B === this.C) || (this.C === this.A)) {
-                    $('#isosceles').append("<li>" + this.A + ", " + this.B + ", " + this.C + "</li>");
-                    return "isosceles triangle";
+                    return "isosceles";
 
                 } else if ((sides[0]*sides[0]) + (sides[1]*sides[1]) === (sides[2]*sides[2])) {
-                    $('#right').append("<li>" + this.A + ", " + this.B + ", " + this.C + "</li>");
-                    return "right triangle";
+                    return "right";
 
                 } else {
-                    $('#scalene').append("<li>" + this.A + ", " + this.B + ", " + this.C + "</li>");
-                    return "scalene triangle";
+                    return "scalene";
                 }
             }
         };
 
         var result = triangle.triangleCheck();
 
+        $('#' + result).append("<li>" + sideA + ", " + sideB + ", " + sideC + "</li>");
+
         $("#sideB").val("");
         $("#sideC").val("");
         $("#sideA").val("").focus();
 
         $("#result").show();
-        $("#error").hide();
 
-        $('.check').text(result);
-
+        if (triangle.isError) {
+            $('.check').text(result);
+        }
+        else {
+            $('.check').text(result + ' triangle');
+        }
         event.preventDefault();
-        });
+    });
 
         $("#clear").click(function(event) {
             $("#result").hide();
@@ -62,7 +64,5 @@ $(document).ready(function() {
             $("#sideC").val("");
             $("#sideA").val("").focus();
 
-        event.preventDefault();
-
- });
+        });
 });
